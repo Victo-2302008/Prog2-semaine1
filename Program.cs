4fb468c7ae8d6a0f4c7dcc6afd60ca69152ed9e4
Frederick Goupil <2302008@carrefour.cegepvicto.ca>
2024-01-22 08:43:14 -0500

    debut exercice

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Linq.Expressions;
+
+namespace semaine1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        /// <summary>
+        /// Afficher "Allo p2" a la console
+        /// </summary>
+        static void Exercice1()
+        {
+            Console.WriteLine("Allo p2");
+        }
+        /// <summary>
+        /// Identifie et imprime deux nombre donner en parametre
+        /// </summary>
+        /// <param name="num1">premier nombre</param>
+        /// <param name="num2">deuxieme nombre</param>
+        static void Exercice2(int num1 ,int num2)
+        {
+            Console.WriteLine("Premier nombre : " + num1 + "\nDeuxieme nombre : " + num2);
+        }
+        /// <summary>
+        /// Additionne deux nombre et retourne le resultat
+        /// </summary>
+        /// <param name="num1">premier nombre</param>
+        /// <param name="num2">deuxieme nombre</param>
+        /// <returns>Resultat de l'addition</returns>
+        static int Exercice3(int num1 ,int num2)
+        {
+            int resultat = num1 + num2;
+            return resultat;
+        }
+        /// <summary>
+        /// Demande un nombre a l'utilisateur et le retourne
+        /// </summary>
+        /// <returns>Nombre demander a l'utilisateur</returns>
+        static int Exercice4()
+        {
+            int nombre;
+            Console.Write("Entrer un nombre : ");
+            int.TryParse(Console.ReadLine(),out nombre);
+
+            return nombre;
+        }
+        /// <summary>
+        /// Demande un nombre superieur ou egal a 0 a l'utilisateur et le retourne
+        /// </summary>
+        /// <remarks>Un nombre invalide retourne -1</remarks>
+        /// <returns></returns>
+        static int Exercice5()
+        {
+            int nombre;
+            Console.Write("Entrer un nombre superieur a 0 : ");
+            int.TryParse(Console.ReadLine(),out nombre);
+            if (nombre < 0)
+            {
+                nombre = -1;
+            }
+            return nombre;
+        }
+        /// <summary>
+        /// Demande a l'utilisateur un nombre jusqu'a ce qu'il soit superieur a 10
+        /// </summary>
+        /// <returns>le nombre valide</returns>
+        static int Exercice6()
+        {
+            int nombre = 0;
+            while (nombre <= 10)
+            {
+                Console.Write("Entrer un nombre superieur a 10");
+                int.TryParse(Console.ReadLine(),out nombre);
+            }
+            return nombre;
+        }
+        /// <summary>
+        /// Divise deux numero donner en parametre et retourne le resultat
+        /// </summary>
+        /// <remarks>Retourne 0 si le denominateur est 0
+        /// <param name="numerateur">Numerateur</param>
+        /// <param name="denominateur">Denominateur</param>
+        /// <returns>Resultat de la division</returns>
+        static int Exercice7(int numerateur ,int denominateur)
+        {
+            int resultat;
+
+            if (denominateur == 0)
+            {
+                resultat = 0;
+            }
+            else
+            {
+                resultat = numerateur / denominateur;
+            }
+
+            return resultat;
+        }
+
+        static 
+    }
+}
